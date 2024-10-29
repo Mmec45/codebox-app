25d30d9d0cb337be224e181939c1dc8ff18d3632
Mmec45 <cyrilmisse1@gmail.com>
2024-10-29 22:06:08 +0100

    feat(auth): complete login screen implementation

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,19 @@
-import { Text, View } from "react-native";
+import {  View , StyleSheet} from "react-native";
+import LoginScreen from "./screens/Login";
+
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <LoginScreen/>
     </View>
   );
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  }
+})
